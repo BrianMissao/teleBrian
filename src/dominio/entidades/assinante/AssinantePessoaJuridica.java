b43420bbc236c62a138e904fd689a0b41f99dd0c
BrianMissao <brianmissao@gmail.com>
2020-08-05 14:15:27 -0400

    Criada uma lógica diferenciada para o método de obter valor do plano específico da classe 'AssinantePessoaJuridica'

--- src/dominio/entidades/assinante/AssinantePessoaJuridica.java
+++ src/dominio/entidades/assinante/AssinantePessoaJuridica.java
@@ -3,12 +3,21 @@ package dominio.entidades.assinante;
 import dominio.entidades.Plano;
 import dominio.excecoesDeRegraDeNegocio.ExcecaoDeArgumentoInvalido;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class AssinantePessoaJuridica extends Assinante {
     private String cnpj;
 
     public AssinantePessoaJuridica(String nome, String cnpj, Plano plano) throws ExcecaoDeArgumentoInvalido {
         super(nome, plano);
         this.cnpj = cnpj;
+    }
+
+    @Override
+    public BigDecimal obterValorDoPlano() {
+        BigDecimal valorDoDesconto = super.obterValorDoPlano().multiply(new BigDecimal("0.25"));
+        return super.obterValorDoPlano().subtract(valorDoDesconto).setScale(2, RoundingMode.HALF_EVEN);
     }
 
     public String getCnpj() {
@@ -21,6 +30,6 @@ public class AssinantePessoaJuridica extends Assinante {
 
     @Override
     public String toString() {
-        return "*"+getNome()+"\nCNPJ: "+getCnpj()+"\nPlano contratado: "+obterNomeDoPlano()+"\nValor do plano contratado: "+obterValorDoPlano();
+        return "*" + getNome() + "\nCNPJ: " + getCnpj() + "\nPlano contratado: " + obterNomeDoPlano() + "\nValor do plano contratado: " + obterValorDoPlano();
     }
 }
