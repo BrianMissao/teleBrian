961fef3358aec032b81758bc7c2179f75f60cbb4
Brian Missao <brianmissao@gmail.com>
2022-12-11 10:16:56 -0400

    Adicionando o primeiro teste de integração

---
+++ src/test/java/com/brian/teleBrian/aplicacao/TesteBase.java
@@ -0,0 +1,14 @@
+package com.brian.teleBrian.aplicacao;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public abstract class TesteBase {
+
+}
